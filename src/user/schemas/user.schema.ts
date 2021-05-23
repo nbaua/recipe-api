83d81f98aaa17c0e311adf270eb63cb5920fa6a7
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-23 18:02:19 +0530

    moved user schema

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Recipe } from '../../filter/schemas/recipe.schema';
+
+@Schema({ collection: 'user', id: true, timestamps: true })
+export class User {
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ type: [{ type: String, ref: Recipe.name }] })
+  favoriteRecipes: Recipe[];
+
+  @Prop({ type: [{ type: String, ref: Recipe.name }] })
+  likedRecipes: Recipe[];
+}
+export const UserSchema = SchemaFactory.createForClass(User);
