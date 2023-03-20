568fe29cfa5abdecbce6a1c7072c81ec965e0aa9
Nhilesh Baua <nhilesh.baua@gmail.com>
2023-03-20 11:36:50 +0530

    Create secured admins as well

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+  constructor(private readonly adminService: AdminService) { }
+
+  @Post()
+  create(@Body() CreateAdminDto: CreateAdminDto) {
+    return this.adminService.create(CreateAdminDto);
+  }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { Connection, Repository } from 'typeorm';
+import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 import { Admin } from './entities/admin.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AdminService {
   private adminRepository: Repository<Admin>;
   constructor(private _connection: Connection) {
     this.adminRepository = this._connection.getRepository(Admin);
+  }
+
+
+  async create(createAdminDto: CreateAdminDto) {
+    const user = this.adminRepository.create();
+    const found = await this.findByEmail(createAdminDto.email);
+
+    if (found === null) {
+      const newUser = this.adminRepository.create();
+
+      newUser.email = createAdminDto.email;
+      newUser.password = bcrypt.hashSync(
+        createAdminDto.password,
+        bcrypt.genSaltSync(10),
+      );
+
+      await this.adminRepository.save(newUser);
+
+      delete user.password;
+      return user;
+    } else return null;
   }
 
   findOne(id: number) {
